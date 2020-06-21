a3b3bf5697b9ab53eaf17684c8a804c8d5c15817
Geovanny Cordero <geovanny@pm.me>
2020-06-20 22:48:50 -0600

    Add user creation validations

--- controllers/user.js
+++ controllers/user.js
@@ -1,31 +1,53 @@
+const makeValidation = require('@withvoid/make-validation')
+
 const userController = {
   getAll: (req, res, next) => {
-    return res.status(200).json({success: true, message: 'get all users'})
+    return res.status(200).json({ success: true, message: 'get all users' })
   },
+
   getById: (req, res, next) => {
     const { id } = req.params
-    return res.status(200).json({success: true, message: `get user with id '${id}'`})
+    return res.status(200).json({ success: true, message: `get user with id '${id}'` })
   },
+
   createUser: (req, res, next) => {
-    const { id, name, email } = req.body
+    const { id, name, email, type } = req.body
+
+    const validation = makeValidation(types => {
+      return {
+        payload: req.body,
+        checks: {
+          name: { type: types.string, options: { empty: false } },
+          email: { type: types.string, options: { empty: false } },
+          type: { type: types.enum, options: { enum: 'admin support user'} },
+        }
+      }
+    })
+    if (!validation.success) {
+      return res.status(400).json({ ...validation })
+    }
+
     const userPayload = {
       id,
       name,
-      email
+      email,
+      type
     }
 
     return res.status(201).json({
-      success: true, 
+      success: true,
       message: `user with id '${id}' created!`,
       data: userPayload
     })
   },
+
   updateUser: (req, res, next) => {
-    const { id, name, email } = req.body
+    const { id, name, email, type } = req.body
     const userPayload = {
       id,
       name,
-      email
+      email,
+      type
     }
     return res.status(200).json({
       success: true,
@@ -33,9 +55,10 @@ const userController = {
       data: userPayload
     })
   },
+
   deleteUser: (req, res, next) => {
     const { id } = req.params
-    return res.status(200).json({success: true, message: `user with id '${id}' deleted!`})
+    return res.status(200).json({ success: true, message: `user with id '${id}' deleted!` })
   },
 }
 
