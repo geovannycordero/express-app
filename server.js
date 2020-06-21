3652516fa8cde8d9d9d5c03d37fc903b4adf0df1
Geovanny Cordero <geovanny@pm.me>
2020-06-20 21:04:17 -0600

    Add user routes and json support, also nested jsons with

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const cors = require('cors')
 const morgan = require('morgan')
 const express = require('express')
 
-const app = express()
+const userRouter = require('./routes/user')
 
+const app = express()
 app.use(cors())
 app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded( { extended: true }))
 
 const port = 3001;
+
+app.use('/users', userRouter)
 
 const server = http.createServer(app)
 server.listen(port)
